94c8764f87f87b450209560115bc21f62016644c
pokus <pokus@notes>
2012-05-02 23:08:49 +0200

    FIX: segmentation faut (2)

--- simconvoi.cc
+++ simconvoi.cc
@@ -2796,8 +2796,6 @@ void convoi_t::self_destruct()
  */
 void convoi_t::destroy()
 {
-	// has to be before schedule is deleted
-	waytype_t wt = get_schedule()->get_waytype();
 	// can be only done here, with a valid convoihandle ...
 	if(fahr[0]) {
 		fahr[0]->set_convoi(NULL);
@@ -2823,7 +2821,11 @@ void convoi_t::destroy()
 	}
 
 	// pay the current value
-	besitzer_p->add_new_vehicle( calc_restwert(), get_pos().get_2d(), wt );
+	if( fahr[0] ) {
+		besitzer_p->add_new_vehicle( calc_restwert(), get_pos().get_2d(), fahr[0]->get_besch()->get_waytype() );
+	} else {
+		besitzer_p->add_new_vehicle( calc_restwert(), get_pos().get_2d(), ignore_wt );
+	}
 
 	for(  uint8 i = anz_vehikel;  i-- != 0;  ) {
 		if(  !fahr[i]->get_flag( ding_t::not_on_map )  ) {

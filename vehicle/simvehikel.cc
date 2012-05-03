23401e7df4bdebd6792c5a66b78cc65857c5a841
pokus <pokus@notes>
2012-05-03 16:59:33 +0200

    FIX: bug in vehicle/ - bad waytype_t to add_transported

--- vehicle/simvehikel.cc
+++ vehicle/simvehikel.cc
@@ -740,7 +740,7 @@ uint16 vehikel_t::unload_freight(halthandle_t halt)
 					// book delivered goods to destination
 					if(end_halt==halt) {
 						// passanger .. 0, mail .. 1, other .. 2
-						get_besitzer()->add_transported( menge, get_waytype(), tmp.get_index() );
+						get_besitzer()->add_transported( menge, get_besch()->get_waytype(), tmp.get_index() );
 					}
 
 					i = fracht.erase( i );
